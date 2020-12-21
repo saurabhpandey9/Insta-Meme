de079686584d563a326823ff7631c153b5f95fb8
Saurabh Pandey <saurabh17100@gmail.com>
2020-12-21 14:39:07 +0530

    client UI started

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from "./components/Navbar";
+import { BrowserRouter,Route } from 'react-router-dom';
+import Home from "./components/screens/Home";
+import Login from "./components/screens/Login";
+import Signup from "./components/screens/Signup";
+import Profile from "./components/screens/Profile";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <NavBar/>
+
+    <Route exact path='/'><Home/></Route>
+    <Route path='/login'><Login/></Route>
+    <Route path='/signup'><Signup/></Route>
+    <Route path='/profile'><Profile/></Route>
+
+
+    </BrowserRouter>
+   
   );
 }
 
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,22 @@
+import react from 'react';
+import { Link } from 'react-router-dom';
+
+
+const NavBar = () => {
+
+    return (
+        <nav>
+            <div className="nav-wrapper orange" >
+                <Link to="/" className="brand-logo left">InstaMeme</Link>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li><Link to="/login">Login</Link></li>
+                    <li><Link to="/signup">Signup</Link></li>
+                    <li><Link to="/profile">Profile</Link></li>
+                </ul>
+            </div>
+        </nav>
+    );
+
+}
+
+export default NavBar;
